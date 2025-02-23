b77313d27e5f81e931d8c8b9163f5d71e3a98dcc
Hayder Ali <hayderbd4290@gmail.com>
2025-02-23 23:19:26 +0600

    send message api created

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,5 +1,36 @@
+import Conversation from "../models/converSation.model.js";
+import Message from "../models/message.model.js";
 
-export const sendMessage = async (req, res)=>{
-     console.log("send message", req.params.id)
+export const sendMessage = async (req, res) => {
+  try {
+    const { message } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
+
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
+    
+    const newMessage = new Message({
+     senderId,
+     receiverId,
+     message
+    })
+
+    if(newMessage){
+     conversation.messages.push(newMessage._id)
+    }
+
+    res.status(201).json(newMessage);
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ error: "internal server error" });
+  }
 };
-
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import { sendMessage } from '../controller/message.controller.js';
+import verifyToken from '../middleWare/verifyToken.js';
 
 const router = express.Router();
 
-router.post('/send/:id', sendMessage)
+router.post('/send/:id',verifyToken, sendMessage)
 
 export default router
