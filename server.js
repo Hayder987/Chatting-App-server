fd6a40afce85161decddd10dd8059c0f7aa26125
Hayder <hayderbd4290@gmail.com>
2025-03-03 20:14:31 +0600

    fix deploy render issue

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
-import path from "path";
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
 
-import authRoutes from './routes/auth.routes.js'
-import userRoutes from './routes/user.routes.js'
-import messageRoutes from './routes/message.routes.js'
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-import connectedToMongoDb from './db/connectedToMongoDB.js'
-import { app, server } from './socket/socket.js'
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 
+import connectedToMongoDb from "./db/connectedToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
-dotenv.config()
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/messages', messageRoutes)
-app.use('/api/users', userRoutes)
-
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
-
-app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-})
+// API Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 
-server.listen(PORT, ()=>{
-    connectedToMongoDb()
-    console.log(`server running At:${PORT}`)
-})
-
+server.listen(PORT, () => {
+  connectedToMongoDb();
+  console.log(`Server running at: ${PORT}`);
+});
